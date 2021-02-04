705c0750fb75d0091a2e100baf30e46d52913b51
felixalbert97 <felixalbert97@web.de>
2021-02-04 19:56:10 +0100

    model.py aufgeräumt, condition Spalte imputed

--- model.py
+++ model.py
@@ -3,82 +3,93 @@ import tools
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import mean_absolute_error as mae
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import AdaBoostRegressor
 
-df = pd.read_csv('data_imputed_1.csv', index_col=0)
+def preprocessing_pipepline(df=None, target=None, features_categorical=None, features_numerical=None, test_set_size=0.3, random_state=1, dummy_trap=True):
+
+    # Selektiere alle relevanten Spalten und reduziere Datensatz 
+    cols = [target] + features_categorical + features_numerical
+    df = df[cols]
+
+    # Erstelle Dummievariablen für kategorische Features
+    df = tools.generate_dummies(df,features_categorical,trap=dummy_trap)
+
+    # Erstelle Training- und Test-set
+    X = df.loc[:, ~df.columns.isin([target])]
+    y = df.loc[:,[target]]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_set_size, random_state=random_state)
+
+    # Skaliere Features
+    feature_scaler, X_train[features_numerical], X_test[features_numerical] = tools.scale_features(X_train[features_numerical], X_test[features_numerical])
+
+    # Skaliere Zielvariable (füge y_train und y_test eine skalierte Spalte hinzu)
+    target_scaler, y_train, y_test = tools.scale_targets(y_train, y_test)
+
+    return X_train, X_test, y_train, y_test, target_scaler
 
 
-###################
-# Feature Auswahl #
-###################
+def model_scores(model = None, X_train=None, X_test=None, y_train=None, y_test=None, target_scaler=None, scale_target=True):
 
-# alle relevanten Spalten
-cols = ['baseRent', 'livingSpace','garden', 'noRooms', 'yearConstructedRange_new', 'interiorQual_new']
-# Davon kategorische features:
-features_categorical = ['yearConstructedRange_new','noRooms', 'interiorQual_new', 'garden']
-# Davon numerische features:
-features_numerical = ['livingSpace']
+    # Erstelle Kopien der Daten, damit sie nicht verändert werden
+    X_train_copy, X_test_copy, y_train_copy, y_test_copy = X_train.copy(), X_test.copy(), y_train.copy(), y_test.copy()
 
-# Reduziere Datensatz auf betrachtete Stadt
-#city = 'Hamburg'
-#df = df.loc[df.regio2 == city]
+    # Berechne Vorhersagen für skalierte oder unskalierte Zielvariable
+    if scale_target:
+        model.fit(X_train_copy,y_train_copy[['target_sc']])
+        y_test_copy['predict_sc'] = model.predict(X_test_copy)
+        y_test_copy['prediction'] = target_scaler.inverse_transform(y_test_copy['predict_sc'])
+    else:
+        model.fit(X_train_copy,y_train_copy[target])
+        y_test_copy['prediction'] = model.predict(X_test_copy)
 
-# Reduziere Datensatz auf relevante Spalten
-df = df[cols]
+    # Modellgüte berechnen 
+    mae_score = mae(y_test_copy[target], y_test_copy['prediction'])
+    mse_score = mse(y_test_copy[target], y_test_copy['prediction'], squared=False)
+    R2_score = model.score(X_test_copy, y_test_copy['target_sc'])
 
-
-#################
-# Preprocessing #
-#################
-
-# Erstelle Dummievariablen für kategorische Features
-df = tools.generate_dummies(df,features_categorical,trap=True)
-
-# Split training- und test-set
-X = df.loc[:, ~df.columns.isin(['baseRent'])]
-y = df.loc[:,['baseRent']]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=1)
-
-# Skaliere Zielvariable (füge y_train und y_test eine skalierte Spalte hinzu)
-sc_baseRent, y_train_sc, y_test_sc = tools.scale_targets(y_train, y_test)
-
-# Skaliere Features
-sc_features, X_train[features_numerical], X_test[features_numerical] = tools.scale_features(X_train[features_numerical], X_test[features_numerical])
+    results = [mae_score, mse_score, R2_score]
+    return results
 
 
-########################
-# Modelle im Vergleich #
-########################
+if __name__ == "__main__":
 
-# Lineare Regression
-X_train_lg, X_test_lg, y_train_lg, y_test_lg = X_train.copy(), X_test.copy(), y_train_sc.copy(), y_test_sc.copy()
-lg = LinearRegression()
-lg.fit(X_train_lg,y_train_lg[['target_sc']])
-y_test_lg['predict_sc'] = lg.predict(X_test_lg)
-y_test_lg['prediction'] = sc_baseRent.inverse_transform(y_test_lg['predict_sc'])
+    ###############
+    # Import Data #
+    ###############
+    df = pd.read_csv('data_imputed_1.csv', index_col=0)
 
-# Modellgüte berechnen durch RMSE
-score_lg = mse(y_test_lg['baseRent'], y_test_lg['prediction'], squared=False)
-# Modellgüte berechnen durch R^2 Wert
-R2_lg = lg.score(X_test_lg, y_test_lg['target_sc'])
+    ###################
+    # Feature Auswahl #
+    ###################
 
-# DecisionTree 
-X_train_rfr, X_test_rfr, y_train_rfr, y_test_rfr = X_train.copy(), X_test.copy(), y_train_sc.copy(), y_test_sc.copy()
-rfr = RandomForestRegressor(max_depth=30, random_state=1)
-rfr.fit(X_train_rfr,y_train_rfr[['target_sc']])
-y_test_rfr['predict_sc'] = rfr.predict(X_test_rfr)
-y_test_rfr['prediction'] = sc_baseRent.inverse_transform(y_test_rfr['predict_sc'])
+    # Zielvariable
+    target = 'baseRent'
+    # Kategorische features:
+    features_categorical = ['yearConstructedRange_new','noRooms', 'interiorQual_new', 'condition_new']
+    # Numerische features:
+    features_numerical = ['livingSpace']
 
-# Modellgüte berechnen durch RMSE
-score_rfr = mse(y_test_rfr['baseRent'], y_test_rfr['prediction'], squared=False)
+    #################
+    # Preprocessing #
+    #################
 
+    X_train, X_test, y_train, y_test, target_scaler = preprocessing_pipepline(df=df, target=target, features_categorical=features_categorical, features_numerical=features_numerical, test_set_size=0.3, random_state=1)
 
-# AdaBoost (Beispiel ohne Skalierung)
-X_train_ada, X_test_ada, y_train_ada, y_test_ada = X_train.copy(), X_test.copy(), y_train_sc.copy(), y_test_sc.copy()
-ada = AdaBoostRegressor(learning_rate=0.5, random_state=1)
-ada.fit(X_train_ada,y_train_ada['baseRent'])
-y_test_ada['prediction'] = ada.predict(X_test_ada)
+    ################
+    # Modellscores #
+    ################
 
-# Modellgüte berechnen durch RMSE
-score_ada = mse(y_test_ada['baseRent'], y_test_ada['prediction'], squared=False)
+    #Lineare Regression
+    lg = LinearRegression()
+    results_lg = model_scores(model=lg, X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, target_scaler=target_scaler)
+
+    #RandomForest
+    rf = RandomForestRegressor(max_depth=30, random_state=1)
+    results_rf = model_scores(model=rf, X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, target_scaler=target_scaler)
+
+    #AdaBoost
+    ada = AdaBoostRegressor(learning_rate=0.3, n_estimators=200, random_state=1)
+    results_ada = model_scores(model=ada, X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test, target_scaler=target_scaler)
+
